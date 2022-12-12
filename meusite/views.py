c12a2ca75eabf7fa94ede2fc22db734c9fdd9bc9
rodrigo pestana <pontobitmap@gmail.com>
2022-12-11 23:44:46 -0300

    arquivos

---
+++ meusite/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+
+class IndexView(TemplateView):
+
+    template_name = "meusite/index.html"
+
+index = IndexView.as_view()
--- pontobitmap/wsgi.py
+++ pontobitmap/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pontobitmap.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pontobitmap.settings")
 
 application = get_wsgi_application()
